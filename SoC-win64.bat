5aed861a592cd31fa1c77464c4a744da33c59922
Benoît Robin <brobin@ddom.ad.corp>
2020-05-15 23:31:55 +0200

    Complete winxx bat scripts
    Signed-off-by: Benoît Robin <brobin@ddom.ad.corp>

--- SoC-win64.bat
+++ SoC-win64.bat
@@ -1,57 +1,37 @@
 @ECHO OFF 
 REM Change to the integration folder
 SET CurrentDir=%~dp0
-CD SoC-Integration-Windows
+CD Integration
 
 REM Create links to naev repository's folders
-RMDIR .\bin
-MKLINK /J  .\bin                ..\..\Repositories\naev\bin
-RMDIR .\build
-MKLINK /J  .\build              ..\..\Repositories\naev\build
-RMDIR .\docs
-MKLINK /J  .\docs               ..\..\Repositories\naev\docs
-RMDIR .\extras
-MKLINK /J  .\extras             ..\..\Repositories\naev\extras
-RMDIR .\lib
-MKLINK /J  .\lib                ..\..\Repositories\naev\lib
-RMDIR .\m4
-MKLINK /J  .\m4                 ..\..\Repositories\naev\m4
-RMDIR .\po
-MKLINK /J  .\po                 ..\..\Repositories\naev\po
-RMDIR .\src
-MKLINK /J  .\src                ..\..\Repositories\naev\src
-RMDIR .\.travis
-MKLINK /J  .\.travis            ..\..\Repositories\naev\.travis
-RMDIR .\utils
-MKLINK /J  .\utils              ..\..\Repositories\naev\utils
+FOR /D %%F in (*) do (
+	IF NOT "%%~nF"=="" (
+		RMDIR .\%%~nF
+	)
+)
+FOR /D %%F in (..\Repositories\naev\?*) do (
+	IF NOT "%%~nF"=="" (
+		MKLINK /J  .\%%~nF ..\Repositories\naev\%%~nF
+	)
+)
 
-REM COPY naev repository's files
-COPY /Y  ..\..\Repositories\naev\appveyor.yml       .\appveyor.yml
-COPY /Y  ..\..\Repositories\naev\AUTHORS            .\AUTHORS
-COPY /Y  ..\..\Repositories\naev\autogen.sh         .\autogen.sh
-COPY /Y  ..\..\Repositories\naev\CHANGELOG          .\CHANGELOG
-COPY /Y  ..\..\Repositories\naev\configure.ac       .\configure.ac
-COPY /Y  ..\..\Repositories\naev\datapath.lua       .\datapath.lua
-COPY /Y  ..\..\Repositories\naev\LICENSE            .\LICENSE
-COPY /Y  ..\..\Repositories\naev\Makefile.am        .\Makefile.am
-COPY /Y  ..\..\Repositories\naev\naev.6             .\naev.6
-COPY /Y  ..\..\Repositories\naev\naev.appdata.xml   .\naev.appdata.xml
-COPY /Y  ..\..\Repositories\naev\naev-confupdate.sh .\naev-confupdate.sh
-COPY /Y  ..\..\Repositories\naev\naev.desktop       .\naev.desktop
-COPY /Y  ..\..\Repositories\naev\README             .\README
-COPY /Y  ..\..\Repositories\naev\Readme.md          .\Readme.md
-COPY /Y  ..\..\Repositories\naev\TODO               .\TODO
-COPY /Y  ..\..\Repositories\naev\.travis.yml        .\.travis.yml
-COPY /Y  ..\..\Repositories\naev\SoC-NAEV-win32.bat .\SoC-NAEV-win32.bat
-COPY /Y  ..\..\Repositories\naev\SoC-NAEV-win64.bat .\SoC-NAEV-win64.bat
+REM Copy naev repository's files
+FOR %%F in (*) do (
+)
+FOR %%F in (..\Repositories\naev\*) do (
+	COPY /Y  %%F .
+)
 
-REM Links SoC-data to local SoC-data repository
-RMDIR .\SoC-data
-MKLINK /J .\SoC-data                                ..\SoC-data
+REM Use datapath.lua to use local NAEV-data folder
+DEL datapath.lua
+echo datapath="../appdata-SoC" > datapath.lua
 
-REM Links dat to SoC-dat repository
-RMDIR .\dat
-MKLINK /J .\dat                                     ..\..\Repositories\SoC-dat
+REM Links dat to the SoC-dat repository itelf
+RMDIR dat
+MKLINK /J .\dat ..\Repositories\SoC-dat
+
+REM Actually launch NAEV in SoC-lin64 configuration
+.\bin\win64\naev.exe
 
 REM restore current folder
 CD %CurrentDir%
