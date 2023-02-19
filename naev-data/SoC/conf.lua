ba4e075a0f2aee7306748e6385535ac704c8389c
U-DDOM\brobin <brobin@DLFRFRA116140.ddom.ad.corp>
2023-02-19 12:26:49 +0100

    Fixed error (2023-02-19_11-35-12_stderr.txt) by creating events/news/core_league.lua
    Signed-off-by: U-DDOM\brobin <brobin@DLFRFRA116140.ddom.ad.corp>

--- naev-data/SoC/conf.lua
+++ naev-data/SoC/conf.lua
@@ -179,7 +179,7 @@ lastversion = "0.10.4+dev"
 translation_warning_seen = true
 
 -- Time Naev was last played. This gets refreshed each time you exit Naev.
-last_played = 1676803146
+last_played = 1676805802
 
 -- Enables FPU exceptions - only works on DEBUG builds
 fpu_except = false
