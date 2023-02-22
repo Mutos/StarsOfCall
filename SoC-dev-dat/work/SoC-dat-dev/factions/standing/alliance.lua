37593e43b3b33137d8ad1d363a37aacf4cdc3233
Mutos <benoit.robin@hoshikaze.net>
2023-02-22 20:22:22 +0100

    Functionnal but not relevant faction "Planetarist Alliance"
    Signed-off-by: Mutos <benoit.robin@hoshikaze.net>

---
+++ SoC-dev-dat/work/SoC-dat-dev/factions/standing/alliance.lua
@@ -0,0 +1,14 @@
+-- Aliance faction standing script
+local sbase = require "factions.standing.lib.base"
+standing = sbase.newStanding( require("factions.alliance") )
+
+local sec_hit_min = 10
+
+function standing:hit( current, amount, source, secondary )
+   local start_standing = self.fct:playerStanding()
+   local f = sbase.Standing.hit( self, current, amount, source, secondary )
+   if secondary and amount < 0 and f < sec_hit_min then
+      f = math.min( start_standing, sec_hit_min )
+   end
+   return f
+end
